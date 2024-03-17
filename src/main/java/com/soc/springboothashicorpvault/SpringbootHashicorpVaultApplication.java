58298722294d57e6ff81b66d4bcd472b623093e2
Imran <isthatimran@gmail.com>
2024-03-17 17:02:34 +0530

    Secret fetch from Vault and establish db connection

--- src/main/java/com/soc/springboothashicorpvault/SpringbootHashicorpVaultApplication.java
+++ src/main/java/com/soc/springboothashicorpvault/SpringbootHashicorpVaultApplication.java
@@ -2,21 +2,28 @@ package com.soc.springboothashicorpvault;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.core.env.Environment;
 import org.springframework.vault.authentication.TokenAuthentication;
 import org.springframework.vault.client.VaultEndpoint;
 import org.springframework.vault.core.VaultTemplate;
 import org.springframework.vault.support.Versioned;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.HashMap;
 import java.util.Map;
 
 @SpringBootApplication
 @EnableConfigurationProperties(MyConfiguration.class)
 public class SpringbootHashicorpVaultApplication implements CommandLineRunner {
+	
+	@Autowired
+	Environment env;
 	
 	Logger log = LoggerFactory.getLogger(SpringbootHashicorpVaultApplication.class);
 	
@@ -37,6 +44,23 @@ public class SpringbootHashicorpVaultApplication implements CommandLineRunner {
 		log.info("Values from Vault Server");
 		log.info("Username " + myConfiguration.getUsername());
 		log.info("Password " + myConfiguration.getPassword());
+		
+		Connection conn =null;
+		
+		String url = env.getProperty("DB_URL");
+		log.info("URL IS "+url);
+		String user =myConfiguration.getUsername();
+		String password=myConfiguration.getPassword();
+		try {
+			Class.forName(env.getProperty("JDBC_DRIVER"));
+			log.info("---DRIVER LOADED SUCCESFULLY---");
+			conn=DriverManager.getConnection(url,user,password);
+			log.info("Connection establish successfuly");
+		}
+		catch(Exception e)
+		{
+			log.info(e.getMessage());
+		}
 
 
 		
